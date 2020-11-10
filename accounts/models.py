49ed7f714d0b3543540e1d04b7b6987e41300c85
Krishm.910 <patelkrishm@gmail.com>
2020-11-10 00:37:56 -0800

    Changes new

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Room(models.Model):
     possible_room_types = [('KS', 'King Smoking'), ('KNS', 'King Non-Smoking')]
     num = models.CharField(max_length=5)
-    room_type = models.CharField(choices=possible_room_types, max_length=3, default=None)
-    check_out = models.BooleanField(default=None)
-    room_clean = models.BooleanField(default=None)
+    room_type = models.CharField(choices=possible_room_types, max_length=3, default=None, null=True)
+    check_out = models.BooleanField(default=None, blank=True, null=True)
+    room_clean = models.BooleanField(default=None, blank=True, null=True)
 
     class Meta:
         db_table = "room"
